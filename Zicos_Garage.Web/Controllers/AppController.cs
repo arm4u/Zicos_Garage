ef20602e81138245a41fa2d3568613ba8d47c3c9
armin.lizde <armin.lizde@outlook.com>
2019-02-08 14:06:29 +0100

    Setting up project I

---
+++ Zicos_Garage.Web/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zicos_Garage.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Zicos_Garage.Web/Startup.cs
+++ Zicos_Garage.Web/Startup.cs
@@ -1,33 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Zicos_Garage.Web
 {
+    /// <summary>
+    /// Startup class
+    /// </summary>
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // dependancy injection (default by microsoft)
+            // service changeable later but DI has to be used
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // developer use only
+            // goto: project -> properties -> enviroment variables
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            // enables using static files
+            app.UseStaticFiles();
+
+            // enables using MVC - Model View Controllers
+            app.UseMvc(configuration => {
+
+                // default behaviour
+                configuration.MapRoute("Default", "{controller}/{action}/{id?}", 
+                    new { controller = "App", Action = "Index" });
             });
         }
     }

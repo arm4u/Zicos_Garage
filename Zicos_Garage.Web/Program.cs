6d1f7c86c96a581cd2e27da2a8fe325aa2c5b8e0
arm4u <38892179+arm4u@users.noreply.github.com>
2019-02-08 14:19:06 +0100

    Update Program.cs
    Arman changed!

--- Zicos_Garage.Web/Program.cs
+++ Zicos_Garage.Web/Program.cs
@@ -16,6 +16,7 @@ namespace Zicos_Garage.Web
         {
             CreateWebHostBuilder(args).Build().Run();
         }
+        
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
